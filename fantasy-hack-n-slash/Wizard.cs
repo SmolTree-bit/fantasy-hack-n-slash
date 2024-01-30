02ff6ec552fb4c9f1bd67488e7031e9781023f14
SmolTree-bit <enok.hunhammer.v3@gmail.com>
2024-01-30 11:49:13 +0100

    three character classes

---
+++ fantasy-hack-n-slash/Wizard.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fantasy_hack_n_slash
+{
+    internal class Wizard
+    {
+
+    }
+}
