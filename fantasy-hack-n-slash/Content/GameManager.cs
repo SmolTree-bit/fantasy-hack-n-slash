c1f6f00824a3c7eeebfdbaa7cac71f58b8b8532f
SmolTree-bit <enok.hunhammer.v3@gmail.com>
2024-02-01 09:53:47 +0100

    game manager added

---
+++ fantasy-hack-n-slash/Content/GameManager.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fantasy-hack-n-slash.Content
+{
+    internal class GameManager
+{
+}
+}
