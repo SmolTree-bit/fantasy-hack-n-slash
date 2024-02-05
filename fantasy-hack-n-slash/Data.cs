08af10c25d99a7c150239fcea6cb925e8eb2cc53
DemonSlayer69696969 <rutgerfischer500@gmail.com>
2024-02-05 14:53:41 +0100

    allt fixat
    stoffes saker

--- fantasy-hack-n-slash/Data.cs
+++ fantasy-hack-n-slash/Data.cs
@@ -17,15 +17,24 @@ namespace fantasy_hack_n_slash
 
         // If this file is edited notify everyone to update
         public static KeyboardState keyboard;
+        public static MouseState mouse;
+
 
         public static Random rnd = new Random();
 
         public static Texture2D
+
             hitBoxImage,
-            tilesetTexture;
+            tilesetTexture,
+            pixelTexture,
+            startGame,
+            endGame;
+
         public static void DataUpdate(GameTime gametime)
         {
             keyboard = Keyboard.GetState();
+            mouse = Mouse.GetState();
+
         }
 
         public static SpriteFont gameFont;
